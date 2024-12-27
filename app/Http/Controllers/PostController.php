4cb439e7ccbc2a754077ecb30d0d46a3fa0628f4
truonguy <truonguy123tx@gmail.com>
2024-12-27 17:57:04 +0700

    import Dowload attachments on the post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+
     /**
      * Store a newly created resource in storage.
      */
@@ -20,53 +21,12 @@ class PostController extends Controller
     {
         $data = $request->validated();
         $user = $request->user();
-        DB::beginTransaction();
-        $allFiles = [];
-        try {
-            $post = Post::create($data);
-            $files = $data['attachments'] ?? [];
-            foreach ($files as $file) {
-                $path = $file->store('attachments/' . $post->id, 'public');
-                $allFilePaths[] = $path;
-                PostAttachment::create([
-                    'post_id' => $post->id,
-                    'name' => $file->getClientOriginalName(),
-                    'path' => $path,
-                    'mime' => $file->getMimeType(),
-                    'size' => $file->getSize(),
-                    'created_by' => $user->id
-                ]);
-            }
-            DB::commit();
-        } catch (\Exception $e) {
-            foreach ($allFilePaths as $path) {
-                Storage::disk('public')->delete($path);
-            }
-            DB::rollBack();
-            throw $e;
-        }
 
-        return back();
-    }
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdatePostRequest $request, Post $post)
-    {
-        $user = $request->user();
         DB::beginTransaction();
         $allFilePaths = [];
         try {
-            $data = $request->validated();
-            $post->update($data);
-            $deleted_ids = $data['deleted_file_ids'] ?? []; // 1, 2, 3, 4
-            $attachments = PostAttachment::query()
-                ->where('post_id', $post->id)
-                ->whereIn('id', $deleted_ids)
-                ->get();
-            foreach ($attachments as $attachment) {
-                $attachment->delete();
-            }
+            $post = Post::create($data);
+
             /** @var \Illuminate\Http\UploadedFile[] $files */
             $files = $data['attachments'] ?? [];
             foreach ($files as $file) {
@@ -81,6 +41,7 @@ class PostController extends Controller
                     'created_by' => $user->id
                 ]);
             }
+
             DB::commit();
         } catch (\Exception $e) {
             foreach ($allFilePaths as $path) {
@@ -89,18 +50,80 @@ class PostController extends Controller
             DB::rollBack();
             throw $e;
         }
+
         return back();
     }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $user = $request->user();
+
+        DB::beginTransaction();
+        $allFilePaths = [];
+        try {
+            $data = $request->validated();
+            $post->update($data);
+
+            $deleted_ids = $data['deleted_file_ids'] ?? []; // 1, 2, 3, 4
+
+            $attachments = PostAttachment::query()
+                ->where('post_id', $post->id)
+                ->whereIn('id', $deleted_ids)
+                ->get();
+
+            foreach ($attachments as $attachment) {
+                $attachment->delete();
+            }
+
+            /** @var \Illuminate\Http\UploadedFile[] $files */
+            $files = $data['attachments'] ?? [];
+            foreach ($files as $file) {
+                $path = $file->store('attachments/' . $post->id, 'public');
+                $allFilePaths[] = $path;
+                PostAttachment::create([
+                    'post_id' => $post->id,
+                    'name' => $file->getClientOriginalName(),
+                    'path' => $path,
+                    'mime' => $file->getMimeType(),
+                    'size' => $file->getSize(),
+                    'created_by' => $user->id
+                ]);
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            foreach ($allFilePaths as $path) {
+                Storage::disk('public')->delete($path);
+            }
+            DB::rollBack();
+            throw $e;
+        }
+
+        return back();
+    }
+
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Post $post)
     {
+        // TODO
         $id = Auth::id();
+
         if ($post->user_id !== $id) {
             return response("You don't have permission to delete this post", 403);
         }
+
         $post->delete();
+
         return back();
+    }
+
+    public function downloadAttachment(PostAttachment $attachment)
+    {
+        return response()->download(Storage::disk('public')->path($attachment->path), $attachment->name);
     }
 }
