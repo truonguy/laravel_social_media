2c7ef03132a5d921fd1a83aaac1b73f653a6ef9d
truonguy <truonguy123tx@gmail.com>
2024-12-30 23:16:08 +0700

    import new PostAttachments component

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -18,6 +18,7 @@ import IndigoButton from "@/Components/app/IndigoButton.vue";
 import { ref } from "vue";
 import ReadMoreReadLess from "@/Components/app/ReadMoreReadLess.vue";
 import EditDeleteDropdown from "@/Components/app/EditDeleteDropdown.vue";
+import PostAttachments from './PostAttachments.vue';
 
 const authUser = usePage().props.auth.user;
 const editingComment = ref(null);
@@ -156,33 +157,10 @@ function sendCommentReaction(comment) {
         <div class="grid gap-3 mb-3" :class="[
             post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2'
         ]">
-            <template v-for="(attachment, ind) of post.attachments.slice(0, 4)">
-
-                <div @click="openAttachment(ind)"
-                    class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative cursor-pointer">
-
-                    <div v-if="ind === 3 && post.attachments.length > 4"
-                        class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
-                        +{{ post.attachments.length - 4 }} more
-                    </div>
-
-                    <!-- Download-->
-                    <a @click.stop :href="route('post.download', attachment)"
-                        class="z-20 opacity-0 group-hover:opacity-100 transition-all w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
-                        <ArrowDownTrayIcon class="w-4 h-4" />
-                    </a>
-                    <!--/ Download-->
-
-                    <img v-if="isImage(attachment)" :src="attachment.url" class="object-contain aspect-square" />
-                    <div v-else class="flex flex-col justify-center items-center">
-                        <PaperClipIcon class="w-10 h-10 mb-3" />
-
-                        <small>{{ attachment.name }}</small>
-                    </div>
-                </div>
-            </template>
+        <PostAttachments :attachments="post.attachments" @attachmentClick="openAttachment"/>
         </div>
         <Disclosure v-slot="{ open }">
+        <!-- Like & Comment buttons-->
             <div class="flex gap-2">
                 <button @click="sendReaction"
                     class="text-gray-800 flex gap-1 items-center justify-center  rounded-lg py-2 px-4 flex-1" :class="[
@@ -271,5 +249,3 @@ function sendCommentReaction(comment) {
 
     </div>
 </template>
-
-<style scoped></style>
