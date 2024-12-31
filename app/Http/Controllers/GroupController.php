c44d0e55580012dba6acd760735a4da80e81e31a
truonguy <truonguy123tx@gmail.com>
2024-12-31 19:27:27 +0700

    import sending and accepting invitations to join groups

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,15 +2,20 @@
 namespace App\Http\Controllers;
 use App\Http\Enums\GroupUserRole;
 use App\Http\Enums\GroupUserStatus;
+use App\Http\Requests\InviteUsersRequest;
 use App\Http\Requests\StoreGroupRequest;
 use App\Http\Requests\UpdateGroupRequest;
 use App\Http\Resources\GroupResource;
 use App\Models\Group;
 use App\Models\GroupUser;
+use App\Notifications\InvitationApproved;
+use App\Notifications\InvitationInGroup;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class GroupController extends Controller
 {
@@ -98,5 +103,51 @@ class GroupController extends Controller
         }
 //        session('success', 'Cover image has been updated');
         return back()->with('success', $success);
+    }
+
+    public function inviteUsers(InviteUsersRequest $request, Group $group)
+    {
+        $data = $request->validated();
+        $user = $request->user;
+        $groupUser = $request->groupUser;
+        if ($groupUser) {
+            $groupUser->delete();
+        }
+        $hours = 24;
+        $token = Str::random(256);
+        GroupUser::create([
+            'status' => GroupUserStatus::PENDING->value,
+            'role' => GroupUserRole::USER->value,
+            'token' => $token,
+            'token_expire_date' => Carbon::now()->addHours($hours),
+            'user_id' => $user->id,
+            'group_id' => $group->id,
+            'created_by' => Auth::id(),
+        ]);
+        $user->notify(new InvitationInGroup($group, $hours, $token));
+        return back()->with('success', 'User was invited to join to group');
+    }
+    public function approveInvitation(string $token)
+    {
+        $groupUser = GroupUser::query()
+            ->where('token', $token)
+            ->first();
+        $errorTitle = '';
+        if (!$groupUser) {
+            $errorTitle = 'The link is not valid';
+        } else if ($groupUser->token_used || $groupUser->status === GroupUserStatus::APPROVED->value) {
+            $errorTitle = 'The link is already used';
+        } else if ($groupUser->token_expire_date < Carbon::now()) {
+            $errorTitle = 'The link is expired';
+        }
+        if ($errorTitle) {
+            return \inertia('Error', compact('errorTitle'));
+        }
+        $groupUser->status = GroupUserStatus::APPROVED->value;
+        $groupUser->token_used = Carbon::now();
+        $groupUser->save();
+        $adminUser = $groupUser->adminUser;
+        $adminUser->notify(new InvitationApproved($groupUser->group, $groupUser->user));
+        return redirect(route('group.profile', $groupUser->group))->with('success', 'You accepted to join to group "'.$groupUser->group->name.'"');
     }
 }
--- resources/js/Pages/Group/View.vue
+++ resources/js/Pages/Group/View.vue
@@ -7,6 +7,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import TabItem from "@/Pages/Profile/Partials/TabItem.vue";
 import { useForm } from '@inertiajs/vue3'
 import PrimaryButton from "@/Components/PrimaryButton.vue";
+import InviteUserModal from './InviteUserModal.vue';
 const imagesForm = useForm({
     thumbnail: null,
     cover: null,
@@ -14,6 +15,7 @@ const imagesForm = useForm({
 const showNotification = ref(true)
 const coverImageSrc = ref('')
 const thumbnailImageSrc = ref('')
+const showInviteUserModal = ref(false);
 const authUser = usePage().props.auth.user;
 const isCurrentUserAdmin = computed(() => props.group.role === 'admin')
 const props = defineProps({
@@ -140,7 +142,8 @@ function submitThurmbnailImage() {
                     </div>
                     <div class="flex justify-between items-center flex-1 p-4">
                         <h2 class="font-bold text-lg">{{ group.name }}</h2>
-                        <PrimaryButton v-if="isCurrentUserAdmin">Invite Users</PrimaryButton>
+                        <PrimaryButton @click="showInviteUserModal = true" v-if="isCurrentUserAdmin">Invite Users
+                        </PrimaryButton>
                         <PrimaryButton v-if="!group.role && group.auto_approval">Join to Group</PrimaryButton>
                         <PrimaryButton v-if="!group.role && !group.auto_approval">Request to join</PrimaryButton>
                     </div>
@@ -180,5 +183,6 @@ function submitThurmbnailImage() {
             </div>
         </div>
     </AuthenticatedLayout>
+    <InviteUserModal v-model="showInviteUserModal" />
 </template>
 <style scoped></style>
