3dba4e2022bd2ad2559f385d69b46fcbab6997b0
truonguy <truonguy123tx@gmail.com>
2025-01-02 17:28:57 +0700

    sending notify on the cases
    post create, comment is placed, post is liked, comment is liked

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,13 +11,19 @@ use App\Models\Comment;
 use App\Models\Post;
 use App\Models\PostAttachment;
 use App\Models\Reaction;
+use App\Notifications\CommentCreated;
+use App\Notifications\ReactionAddedOnComment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
 use App\Notifications\CommentDeleted;
+use App\Notifications\PostCreated;
 use App\Notifications\PostDeleted;
+use App\Notifications\ReactionAddedOnPost;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Notification;
 
 class PostController extends Controller
 {
@@ -51,6 +57,11 @@ class PostController extends Controller
             }
 
             DB::commit();
+            $group = $post->group;
+            if ($group) {
+                $users = $group->approvedUsers()->where('users.id', '!=', $user->id)->get();
+                Notification::send($users, new PostCreated($post, $group));
+            }
         } catch (\Exception $e) {
             foreach ($allFilePaths as $path) {
                 Storage::disk('public')->delete($path);
@@ -158,6 +169,11 @@ class PostController extends Controller
                 'user_id' => $userId,
                 'type' => $data['reaction']
             ]);
+
+            if (!$post->isOwner($userId)) {
+                $user = User::where('id', $userId)->first();
+                $post->user->notify(new ReactionAddedOnPost($post, $user));
+            }
         }
         $reactions = Reaction::where('object_id', $post->id)
             ->where('object_type', Post::class)->count();
@@ -179,6 +195,8 @@ class PostController extends Controller
             'user_id' => Auth::id(),
             'parent_id' => $data['parent_id'] ?: null
         ]);
+        $post = $comment->post;
+        $post->user->notify(new CommentCreated($comment));
         return response(new CommentResource($comment), 201);
     }
 
@@ -224,6 +242,11 @@ class PostController extends Controller
                 'user_id' => $userId,
                 'type' => $data['reaction']
             ]);
+
+            if (!$comment->isOwner($userId)) {
+                $user = User::where('id', $userId)->first();
+                $comment->user->notify(new ReactionAddedOnComment($comment->post, $comment, $user));
+            }
         }
         $reactions = Reaction::where('object_id', $comment->id)->where('object_type', Comment::class)->count();
         return response([
