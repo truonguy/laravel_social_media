58a5626fbd8734661c63be2abd0ff23440d7bf5a
truonguy <truonguy123tx@gmail.com>
2025-01-05 17:42:06 +0700

    accessing open AI API generate post content

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,6 +25,7 @@ use App\Notifications\PostDeleted;
 use App\Notifications\ReactionAddedOnPost;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Notification;
+use OpenAI\Laravel\Facades\OpenAI;
 
 class PostController extends Controller
 {
@@ -267,6 +268,25 @@ class PostController extends Controller
         return response([
             'num_of_reactions' => $reactions,
             'current_user_has_reaction' => $hasReaction
+        ]);
+    }
+
+    public function aiPostContent(Request $request)
+    {
+        $prompt = $request->get('prompt');
+        $result = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                [
+                    'role' => 'user',
+                    'content' => "Please generate social media post content based on the following prompt. Generated formatted content with multiple paragraphs. Put hashtags after 2 lines from the main content". PHP_EOL .PHP_EOL. "Prompt: " .PHP_EOL
+                        . $prompt
+                ],
+            ],
+        ]);
+        return response([
+            'content' => $result->choices[0]->message->content
+//            'content' => "\"🎉 Exciting news! We're thrilled to announce that we just released a brand new feature on our app/website! 💥 Get ready to experience the next level of convenience and efficiency with this game-changing addition. 🚀 Try it out now and let us know what you think! 😍 #NewFeatureAlert #UpgradeYourExperience\""
         ]);
     }
 }
